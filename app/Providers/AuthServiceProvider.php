cacd04ea8100749f8faceacc975079b2850c239f
Juan Manuel Almeyda Requejo <17618@contraloria.gob.pe>
2025-11-13 11:29:41 -0500

    Revert: Remove ProcesoPolicy and clean AuthServiceProvider
    This commit removes the ProcesoPolicy.php file and cleans up AuthServiceProvider.php by removing any remaining policy registrations and imports related to policies that have been deleted or reverted.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,12 +4,6 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use App\Models\Proceso; // Import the Proceso model
-use App\Policies\ProcesoPolicy; // Import the ProcesoPolicy
-use App\Models\Hallazgo; // Import the Hallazgo model
-use App\Policies\HallazgoPolicy; // Import the HallazgoPolicy
-use App\Models\Requerimiento; // Import the Requerimiento model
-use App\Policies\RequerimientoPolicy; // Import the RequerimientoPolicy
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,9 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Proceso::class => ProcesoPolicy::class,
-        Hallazgo::class => HallazgoPolicy::class,
-        Requerimiento::class => RequerimientoPolicy::class,
+
     ];
 
     /**
